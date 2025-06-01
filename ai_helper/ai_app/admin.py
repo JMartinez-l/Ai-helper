6211388a7a4d1cb0a1fb0a0eeaf1c50ae51c68c4
JMartinez-l <jmartinezl@usm.cl>
2025-06-01 19:23:26 -0400

    register customuseradmin in admin.py

--- ai_helper/ai_app/admin.py
+++ ai_helper/ai_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('role',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
